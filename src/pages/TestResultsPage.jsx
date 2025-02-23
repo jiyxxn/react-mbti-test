28e770cbb789a306a618ca7c5560356baaaa4804
jinew <jiyunk@naver.com>
2025-02-23 17:49:06 +0900

    🚚 move : 파일명 변경

---
+++ src/pages/TestResultsPage.jsx
@@ -0,0 +1,12 @@
+import TestResultList from '../components/TestResultList';
+
+/**
+ * 테스트 결과 페이지 컴포넌트
+ * TestResultList 컴포넌트를 렌더링하여 테스트 결과 목록을 표시
+ *
+ * @returns {JSX.Element} 테스트 결과 목록을 포함하는 페이지
+ */
+const TestResultsPage = () => {
+  return <TestResultList />;
+};
+export default TestResultsPage;
